b522ed5b30d08f75b9215e269147136f8948786e
Oloishorua <jacoboloishorua@gmail.com>
2023-10-06 02:57:44 +0300

    prints text to the std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - A program that prints text to the standard error
  * Return: 1 if success, otherwise fail
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
