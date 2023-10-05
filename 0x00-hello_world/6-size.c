fdb9eb1d8ddd680514bce0c8e1064ad359b2e420
Oloishorua <jacoboloishorua@gmail.com>
2023-10-06 02:11:39 +0300

    prints size of types depending on pc

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints size of data types on the pc its on
+ * Return: 0 if success, err otherwise
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lli byte(s)\n", sizeof(long long int));
+	printf("size of a float: %f byte(s)\n", sizeof(float));
+	return (0);
+}
