5d23c896a9193f2d33b12d551ae39da935aeef64
Oloishorua <jacoboloishorua@gmail.com>
2023-10-06 01:34:40 +0300

    prints  text using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/*
+ * main - program that prints text using puts function
+ * Return: 0
+ */
 int main(){
-	puts("Programming is like building a multilingual puzzle\n");
-
-	return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
