485219f5173ac3fa4dc5eabf99db472d26f33603
Oloishorua <jacoboloishorua@gmail.com>
2023-10-06 20:51:37 +0300

    prints positive, negative or zero based on n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - A program that indicates whether a random no is +ve, -ve or zero
+ * Return: 0 if success
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ( n>0 )
+	if (n > 0)
 	{
 		printf("%d is positive", n);
 	}
-	else if ( n==0 )
+	else if (n == 0)
 	{
 		printf("%d is zero", n);
 	}
