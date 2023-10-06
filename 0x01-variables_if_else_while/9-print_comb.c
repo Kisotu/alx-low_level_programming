0084ed23f2addd81827e9b1bf55d0800446df61d
Oloishorua <jacoboloishorua@gmail.com>
2023-10-07 01:27:52 +0300

    prints 0-9 combined with other chars

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all possible combos of single digit nos
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int numbz;
+	/*print 0-9*/
+	for (numbz = 0; numbz <= 9; numbz++)
+	{
+		putchar(numbz + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
