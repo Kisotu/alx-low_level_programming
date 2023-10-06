47c35d8f401ccd92021545cf34315fe414698c46
Oloishorua <jacoboloishorua@gmail.com>
2023-10-07 02:19:48 +0300

    prints uniq combinations of nos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - A program that prints combx but unique
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int num1, num2;
+
+	/* nested for loops to print combinations */
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			/*skip similar and num1 should be less than num2*/
+			if (num1 != num2 && num1 < num2)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				if (num1 + num2 != 17)
+				{
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
