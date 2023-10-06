29789b7764e69dc6f87431104e555b1d5da79f7d
Oloishorua <jacoboloishorua@gmail.com>
2023-10-07 01:12:04 +0300

    prints all base 16 members in lower case

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,19 +4,19 @@
  * Return: 0 if success
  */
 int main(void)
-{       
-        char alph;
-        int numbz;
+{
+	char alph;
+	int numbz;
         /*print 0-9*/
         for (numbz = 0; numbz <= 9; numbz++)
-            {
-                putchar(numbz + '0');
-            }
-        /*print a-f */
-        for (alph = 'a'; alph <= 'f'; alph++)
-            {
-                putchar(alph);
-            }
-        putchar('\n');
-        return (0);
+	{
+	       	putchar(numbz + '0');
+	}
+	/*print a-f */
+	for (alph = 'a'; alph <= 'f'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
 }
