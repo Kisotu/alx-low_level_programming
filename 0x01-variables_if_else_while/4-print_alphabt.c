b3f06ebe9661d58700b21f3b5d23eff030fe3f20
Oloishorua <jacoboloishorua@gmail.com>
2023-10-07 00:14:51 +0300

    prints alphabets but omits q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints alphabets but omits q and e
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char alph;
+
+	while (alph <= 'z')
+	{
+		if ( alph != 'q' || alph != 'e')
+		{
+			putchar(alph);
+		}
+		alph++;
+	}
+	return (0);
+}
+
