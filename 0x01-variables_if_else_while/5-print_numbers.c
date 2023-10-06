6bfa97efeae1fcec416fb197de0dfbffd098e496
Oloishorua <jacoboloishorua@gmail.com>
2023-10-07 00:27:35 +0300

    prints numbers from 0-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints 0-10
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 10; numbers++)
+	{
+		printf("%i\n", numbers);
+	}
+	return (0);
+}
