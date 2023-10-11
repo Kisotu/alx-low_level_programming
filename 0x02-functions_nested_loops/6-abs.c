cfe5049aa98461292b544b1c91246f202f4a82fe
Oloishorua <jacoboloishorua@gmail.com>
2023-10-12 02:15:27 +0300

    checks absolute val of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @int: integer to be computed
+ * Return: Absolute value of integer
+ */
+int _abs(int)
+{
+	if (c < 0)
+	{
+		int abs_val;
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (int);
+}
