558a7a1552968cf268e31f714f316cfc2ecdb0b7
Oloishorua <jacoboloishorua@gmail.com>
2023-10-13 02:01:44 +0300

    prints 0-14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,23 +2,22 @@
 
 /**
  * more_numbers - prints 0-14 tenX
+ *
  * Return: no return val
  */
 void more_numbers(void)
 {
-	int j, k;
 
-	for (j = 0; j <= 9; j++)
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
 	{
-		for (k = 0; k <= 14; k++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (k >= 10)
-			{
-				_putchar(k / 10 + '0');
-			}
-			else
-				_putchar(k % 10 + '0');
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
