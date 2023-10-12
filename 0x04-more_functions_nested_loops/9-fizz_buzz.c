6abdf99f4546a3be9bf1c7e3ddc521350063a3db
Oloishorua <jacoboloishorua@gmail.com>
2023-10-13 02:32:49 +0300

    plays the FizzBuzz game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: plays the FizzBuzz game
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 1; k <= 100; k++)
+	{
+		if (k % 3 == 0 && k % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (k % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (k % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", k);
+		}
+	}
+}
