c56e276513b1eef32c9a1aaaf99173fbac30bf6e
Oloishorua <jacoboloishorua@gmail.com>
2023-10-16 23:02:27 +0300

    takes pointer and updates value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *  reset_to_98 - takes the pointer and updates the value
+ *  @n: integer to check
+ *  Return: 0 if success
+ */
+void reset_to_09(int *n)
+{
+	*n = 98;
+}
