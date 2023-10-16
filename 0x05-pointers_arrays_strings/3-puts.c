0964b0d581f8eaeb49646143ec72b87c6dea5a67
Oloishorua <jacoboloishorua@gmail.com>
2023-10-17 01:33:27 +0300

    prints string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str: string to print
+ * Return: length of string
+ */
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
