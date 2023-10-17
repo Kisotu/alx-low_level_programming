82f6678f7d00c10ad37f3e7f4378839b58ef03f9
Oloishorua <jacoboloishorua@gmail.com>
2023-10-17 03:21:08 +0300

    prints second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of string
+ * @str: string to half
+ * Return: 0 if success
+ */
+void puts_half(char *str)
+{
+	int k = 0, j;
+
+	while (str[k] != '\0')
+	{
+		k++;
+	}
+	if (k + 1 % 2 != '0')
+	{
+		j = (k - 1) / 2;
+	}
+	else
+	{
+		j = (k / 2);
+	}
+	j++;
+
+	for (k = j; str[k] != '\0'; k++)
+	{
+		_putchar(str[k]);
+		_putchar('\n');
+	}
+}
