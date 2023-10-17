713c113a497d8c0c500e13ea86e3e96512dde16c
Oloishorua <jacoboloishorua@gmail.com>
2023-10-17 03:12:46 +0300

    prints every other second char of a string from the first

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character, followed by a new line.
+ * puts2 - prints every other 2nd character of a string
  * @str: string input
  * Return: 0 if success
  */
@@ -10,7 +10,11 @@ void puts2(char *str)
 	int k;
 
 	for (k = 0; str[k] != '\0'; k++)
-	if (k % 2 == 0)
-		_putchar(str[k]);
+	{
+		if (k % 2 == 0)
+		{
+			_putchar(str[k]);
+		}
+	}
 	_putchar('\n');
 }
