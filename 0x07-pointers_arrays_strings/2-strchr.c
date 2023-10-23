df29ca4231f96bdfb26e1530e730ad6201685c85
Oloishorua <jacoboloishorua@gmail.com>
2023-10-24 01:04:35 +0300

    returns first occurrence of char in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,23 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	while (*s != '\0')
 	{
-		if (*s != c)
-		{
-			s++;
-		}
-		else
+		if (*s == c)
 		{
 			return (s);
 		}
+		s++;
 	}
-	if (c == '\0')
-	{
-		return (s);
-	}
-	else
-	{
-		return (NULL);
-	}
+	return (NULL):
 }
