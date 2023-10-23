550e66df00208e88aaaca47077ada7fd8cd12144
Oloishorua <jacoboloishorua@gmail.com>
2023-10-24 02:01:14 +0300

    grts length of prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *  _strspn - gets length of a prefix substring
+ *  @s: pointer to string to search
+ *  @accept: poiner to substring to find
+ *  Return: no of bytes of substring
+ */
+unsigned int _strspn(char *s, char *acccept)
+{
+	int k = 0;
+	int m, n;
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		if (s[m] != 32)
+		{
+			for (n = 0; accept[n] != '\0'; n++)
+			{
+				if (s[m] == accept[n])
+				{
+					k++;
+				}
+			}
+		}
+		else
+			return (k);
+	}
+	return (k);
+}
