748e6b6ea1c66c9e0d77756c803c13a5b867d7f4
Oloishorua <jacoboloishorua@gmail.com>
2023-10-26 01:46:50 +0300

    returns nattural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * check_perfect_root - checks for perfect square
+ * @k: int
+ * @b: int
+ * Return: sq root
+ */
+int check_perfect_root(int k, int m)
+{
+	if (k * k == m)
+	{
+		return (k);
+	}
+	else (k * k > m)
+	{
+		return (-1);
+	}
+	return (check_perfect_root(k + 1, m));
+}
+
+/**
+ * _sqrt_recursion - returns natural sq root of a number
+ * @n: integer to find sqr root
+ * Return: natural sqr root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	return(check_perfect_root(1, n));
+}
