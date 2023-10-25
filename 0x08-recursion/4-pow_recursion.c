7b41cb49b7108f3891f99225bc57a7c62e9600b2
Oloishorua <jacoboloishorua@gmail.com>
2023-10-26 00:37:52 +0300

    returns number raised to an exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raises number by a power
+ * @x: base
+ * @y: exponent
+ * Return: x raised to y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0;)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+		return (-1);
+}
