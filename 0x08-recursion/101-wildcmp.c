cfbede32fff17519be797ff0599897702d19e946
Oloishorua <jacoboloishorua@gmail.com>
2023-10-26 21:31:23 +0300

    checks if a string is identical to another

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * string_checker - check if 2 strings are similar
+ * str_chk - check if 2 strings are similar
  * @st1: string1 address
  * @st2: string2 address
  * @j: left index
  * @k: special index
  * Return: 1 if palindrome
  */
-int string_checker(char *st1, char *st2, int j, int k)
+int str_chk(char *st1, char *st2, int j, int k)
 {
 	if (st1[j] == '\0' && st2[k] == '\0')
 	{
@@ -16,16 +16,14 @@ int string_checker(char *st1, char *st2, int j, int k)
 	}
 	else if (st1[j] == st2[k])
 	{
-		return (string_checker(st1, st2, j + 1, k + 1));
+		return (str_chk(st1, st2, j + 1, k + 1));
 	}
 	else if (st1[j] == '\0' && st2[k] == '*')
 	{
-		return (string_checker(st1, st2, j, k + 1));
+		return (str_chk(st1, st2, j, k + 1));
 	}
 	else if (st2[k] == '*')
-	{
-		return (string_checker(st1, st2, j + 1, k) || string_checker(st1, st2, j, k + 1));
-	}
+		return (str_chk(st1, st2, j + 1, k) || str_chk(st1, st2, j, k + 1));
 	return (0);
 }
 
@@ -37,5 +35,5 @@ int string_checker(char *st1, char *st2, int j, int k)
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (string_checker(s1, s2, 0, 0));
+	return (str_chk(s1, s2, 0, 0));
 }
