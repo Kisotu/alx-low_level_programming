62f7459eed3dd06e4e855ebb0ffdf9d45476d27a
Oloishorua <jacoboloishorua@gmail.com>
2023-10-27 19:10:03 +0300

    prints the no of args passed to the program

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,8 @@
  * @argv: argument vector
  * Return: 0 if success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
