872e82afc75d5b32f657003ddecf48c5766e698a
Oloishorua <jacoboloishorua@gmail.com>
2023-10-27 19:50:46 +0300

    prints all args supplied

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * main - prints all arguments it receives each in a new line
+ * @argc: no of arguments
+ * @argv: argiment vector
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	for (int k = 0; k < argc; k++)
+	{
+		printf("%s\n", argv[k]);
+	}
+	return (0);
+}
