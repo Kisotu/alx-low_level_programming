53d86133b76fc911bd109a2c1e59275a697fb482
Oloishorua <jacoboloishorua@gmail.com>
2023-10-27 18:59:29 +0300

    prints the no of args passed to the program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed
+ * 		to the program
+ * @argc: number of arguments passed
+ * @argv: argument vector
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
