47cfb190ee23dbd36a72a3c41579882beb1ba6e8
Oloishorua <jacoboloishorua@gmail.com>
2023-10-27 20:07:46 +0300

    prints all args supplied

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,17 @@
  * @argv: argiment vector
  * Return: 0 if success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	for (int k = 0; k < argc; k++)
+	int k = 0;
+
+	if (argc > 0)
 	{
-		printf("%s\n", argv[k]);
+		while (k < argc)
+		{
+			printf("%s\n", argv[k]);
+			k++;
+		}
 	}
 	return (0);
 }
