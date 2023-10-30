bf3beba439210d09a2147d8fe30383291e45fe1f
Oloishorua <jacoboloishorua@gmail.com>
2023-10-30 22:05:45 +0300

    creates an array of chars and init with a specific  character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,10 +9,24 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *arr = malloc(size * sizeof(char));
-	for (int k = 0; k < size; k++)
+	char *arr;
+	unsigned int k;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+
+	arr = (char *)malloc(sizeof(char) * size);
+
+	if (arr == '\0')
+	{
+		return ('\0');
+	}
+
+	for (k = 0; k < size; k++)
 	{
 		arr[k] = c;
 	}
-	return arr;
+	return (arr);
 }
