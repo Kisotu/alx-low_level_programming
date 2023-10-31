dd90aae899f0f6ca0bc0927ad66fe2cd01f15fd4
Oloishorua <jacoboloishorua@gmail.com>
2023-11-01 02:17:40 +0300

    minor fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,42 +14,25 @@ char *str_concat(char *s1, char *s2)
 	char *comb_str;
 
 	s1_length = s2_length = 0;
-
 	if (s1 != NULL)
-	{
 		j = 0;
 		while (s1[j++] != '\0')
-		{
 			s1_length++;
-		}
-	}
 
 	if (s2 != NULL)
-	{
 		j = 0;
 		while (s2[j++] != '\0')
-		{
 			s2_length++;
-		}
-	}
 
 	length = s1_length + s2_length;
-
 	comb_str = (char *)malloc(sizeof(char) * (length + 1));
 
 	if (comb_str == NULL)
-	{
 		return (NULL);
-	}
-
 	for (j = 0; j < s1_length; j++)
-	{
 		comb_str[j] = s1[j];
-	}
 	for (k = 0; k < s2_length; j++, k++)
-	{
 		comb_str[j] = s2[k];
-	}
 	comb_str[length] = '\0';
 
 	return (comb_str);
