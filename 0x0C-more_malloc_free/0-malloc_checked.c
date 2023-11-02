89729b7c83cf80d2eca78aef5f9619164d4fa285
Kisotu <kisotujake@gmail.com>
2023-11-03 00:29:01 +0300

    funct that allocates mem using the malloc function

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - func that allocates mem using malloc
+ * @b: memory size to allocate
+ * Return: pointer to the alloc memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *k;
+
+	k = malloc(b);
+	if (k == NULL)
+	{
+		exit (98);
+	}
+	return (k);
+}
