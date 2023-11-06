e4b855397d6c1b0d286e8dbaafa1d66f21055d2d
Kisotu <kisotujake@gmail.com>
2023-11-07 01:43:47 +0300

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,56 +2,38 @@
 #include <string.h>
 #include "dog.h"
 
-dog_t *create_and_initialize_dog(char *name, float age, char *owner)
-{
-	dog_t *doge = malloc(sizeof(dog_t));
-	if (doge == NULL)
-	{
-		return (NULL);
-	}
-
-	doge->name = strdup(name);
-	doge->age = age;
-	doge->owner = strdup(owner);
-
-	return (doge);
-}
-
-int calculate_length(char *str)
-{
-	int i = 0;
-	while (str[i] != '\0')
-		i++;
-	return i;
-}
-
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: owner of dog
+ * Return: new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int name_length = calculate_length(name);
-	int owner_length = calculate_length(owner);
-
-	dog_t *doge = create_and_initialize_dog(name, age, owner);
-	if (doge == NULL)
+	dog_t *dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 	{
+		return{ (NULL);
+	}
+
+	dog->name = strdup(name);
+	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
 	}
 
-	doge->name = realloc(doge->name, (name_length + 1) * sizeof(char));
-	if (doge->name == NULL)
+	dog->age = age;
+
+	dog->owner = strdup(owner);
+	if (dog->owner == NULL)
 	{
-		free(doge->owner);
-		free(doge);
+		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
 
-	doge->owner = realloc(doge->owner, (owner_length + 1) * sizeof(char));
-	if (doge->owner == NULL)
-	{
-		free(doge->name);
-		free(doge);
-		return (NULL);
-	}
-
-	return (doge);
+	return (dog);
 }
 
