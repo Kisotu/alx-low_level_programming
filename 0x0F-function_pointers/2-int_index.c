f3abb6929bedc036e05eb5bc950a290ddd309b99
Kisotu <kisotujake@gmail.com>
2023-11-08 23:09:11 +0300

    fxn that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - fxn that searches for an integer
+ * @array: the array to search
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ * Return: index of the first elem
+ */
+int int_index(int *array, int size, int(*cmp)(int))
+{
+	int k;
+
+	if (array && cmp)
+	{
+		for (k = 0; k < size; k++)
+		{
+			if (cmp(array[k] != 0)
+				return (k);
+		}
+	}
+
+	return (-1);
+}
