5631940cd8cb06f19b6cd89243c314520b02157f
Oloishorua <jacoboloishorua@gmail.com>
2023-11-09 20:42:42 +0300

    adds all args provied by user and returns sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - fxn that adds numbers provided by user
+ * @n: sum parameters
+ * Return: sum of the integers
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int k, sum;
+	va_list vlist;
+
+	sum = 0;
+	if (n == 0)
+		return (0);
+
+	va_start(vlist, n);
+
+	for (k = 0; k < n; k++)
+	{
+		sum += va_arg(vlist, int);
+	}
+	va_end(vlist);
+	
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_HEADER
+#define VARIADIC_HEADER
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
