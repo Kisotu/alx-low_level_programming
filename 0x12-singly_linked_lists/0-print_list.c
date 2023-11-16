e42036c1a9e4d04a2a92295b7a8085dbc24e8344
Oloishorua <jacoboloishorua@gmail.com.com>
2023-11-17 00:24:42 +0300

    function that returns all elems of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_list - A function that prints all elems of a list
+ * @h: name of the linked list
+ * Return: no of nodes
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	int counter;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->length, h->str);
+		}
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LINKED_LIST_HEADER
+#define LINKED_LIST_HEADER
+
+/**
+ * struct list_s - A singly linked list
+ * @str: string that has been allocated mem space
+ * @next: points to the next node
+ * @length: The length of the string
+ * Description: Template for our linked list
+ */
+struct Node
+{
+	unsigned long int length;
+	char *str;
+	struct Node *next;
+};
+typedef struct Node list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
