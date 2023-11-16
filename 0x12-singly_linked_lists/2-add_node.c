01176119172d73fbf0eeccb01b6cae1f4d96c33c
Oloishorua <jacoboloishorua@gmail.com.com>
2023-11-17 02:03:19 +0300

    function that adds new node at the front of list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,21 +12,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *data_new;
-    size_t nth_char;
+	list_t *data_new;
+	size_t nth_char;
 
-    data_new = malloc(sizeof(list_t));
-    if (data_new == NULL)
-        return (NULL);
-   
-    data_new->str = strdup(str);
-
-    for (nth_char = 0; str[nth_char]; nth_char++)
-        ;
-
-    data_new->len = nth_char;
-    data_new->next = *head;
-    *head = data_new;
-
-    return (*head);
+	data_new = malloc(sizeof(list_t));
+	if (data_new == NULL)
+		return (NULL);
+	
+	data_new->str = strdup(str);
+	
+	for (nth_char = 0; str[nth_char]; nth_char++)
+		;
+	
+	data_new->len = nth_char;
+	data_new->next = *head;
+	*head = data_new;
+	
+	return (*head);
 }
