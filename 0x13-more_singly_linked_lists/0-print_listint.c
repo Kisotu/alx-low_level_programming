6530dc4b786706a41c79e02751da9e795c091cbc
Kisotu <jacoboloishorua@gmail.com.com>
2023-11-21 15:49:47 +0300

    function that returns the no of nodes in a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,17 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t no_of_nodes = 0;
+	size_t no_of_nodes = 1;
 
 	if (h == NULL)
 		return (0);
-
-	for (h; h != NULL; h = h->next)
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
+		h = h->next;
 		no_of_nodes++;
 	}
+	printf("%d\n", h->n);
 
 	return (no_of_nodes);
 }
