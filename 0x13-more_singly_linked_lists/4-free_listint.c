5ba04884d935dc92ee88b34da7658d95e2ed91d5
Kisotu <jacoboloishorua@gmail.com.com>
2023-11-21 23:48:00 +0300

    function that frees memory

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists"
+
+/**
+ * free_listint - function that frees list's memory
+ * @head: pointer to head
+ * Return: void
+ */
+void free_listint(listit_t *head)
+{
+	listint_t *node;
+
+	while ((node == head) != NULL)
+	{
+		head = ndoe->next;
+		free(node);
+	}
+}
