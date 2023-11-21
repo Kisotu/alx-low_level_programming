9a117fdf77b4564d6d7efbe77e54600371f20ed0
Kisotu <jacoboloishorua@gmail.com.com>
2023-11-21 15:33:24 +0300

    function that returns the no of nodes in a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints all the elms of a list
+ * @h: the pointer to head
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t no_of_nodes = 1;
+
+	if (h == NULL)
+		return (0);
+
+	for (const listint_t *node = h; node != NULL; node = node->next)
+	{
+		printf("%d\n", node->n);
+		no_of_nodes++;
+	}
+
+	return (nof_of_nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Advanced Singly Linked Lists in C
