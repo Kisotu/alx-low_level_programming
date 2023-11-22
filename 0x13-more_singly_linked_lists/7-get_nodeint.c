aaa5c7fce5116310724e890308e3999948f1fff9
Kisotu <jacoboloishorua@gmail.com.com>
2023-11-22 15:40:13 +0300

    fxn that returns node at nth index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - fxn that gets the node at nth index
+ * @head: pointer to head
+ * @index: index of list to access
+ * Return: the nth node item in list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int k;
+
+	for (k = 0; k <index && head != NULL; k++)
+	{
+		head = head->next;
+	}
+
+	return (head);
+}
