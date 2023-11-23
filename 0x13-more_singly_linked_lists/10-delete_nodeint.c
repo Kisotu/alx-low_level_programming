f730e84467f3ff1000f7ba587c82621e709429e9
Kisotu <jacoboloishorua@gmail.com.com>
2023-11-23 03:02:17 +0300

    fxn that deletes node at given index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - fxn that deletes node at nth index
+ * @head: pointer to had node
+ * @index: index of node to delete
+ * Return: 1 if success, 0 otherwise
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *next_Node, *temp_Node;
+	unsigned int indx;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		next_Node = (*head)->next;
+		free(*head);
+		*head = next_Node;
+		return (1);
+	}
+	temp_Node = *head;
+
+	for (indx = 0; indx < index - 1; indx++)
+	{
+		if (temp_Node->next == NULL)
+			return (-1);
+		temp_Node = temp_Node->next;
+	}
+	next_Node = temp_node->next;
+	temp_Node->next = next_Node->next;
+	free(next_Node);
+	return (1);
+}
