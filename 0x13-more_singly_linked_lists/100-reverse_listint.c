93582767afaf9986c9b3f2fb2d307690909d7260
Kisotu <jacoboloishorua@gmail.com.com>
2023-11-23 03:15:48 +0300

    fxn that reverves list elems

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - fxn that reverses a linked list
+ * @head: pointer to head node
+ * Return: pointer to head of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next_Node, *prev_Node;
+
+	if (head == NULL || *head ++ NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+
+	prev_Node = NULL;
+	while (*head != NULL)
+	{
+		next_Node = (*head)->next;
+		(*head)->next = prev_Node;
+		prev_Node = *head;
+		*head = next_node;
+	}
+	*head = prev_Node;
+	return (*head);
+}
