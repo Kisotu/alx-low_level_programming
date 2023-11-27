ff24798107d4b2d6ab761d05febcc668f6784f48
Oloishorua <jacoboloishorua@gmail.com>
2023-11-28 01:46:54 +0300

    A fxn that appends text to end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)		
+	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 		{
