7b03c74c513b824f305af9e1bb90262a68ab040e
Oloishorua <jacoboloishorua@gmail.com>
2023-11-28 02:42:07 +0300

    program that copies from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,12 +12,12 @@ void file_check(int f_from, int f_to, char *argv[])
 {
 	if (f_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (f_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
