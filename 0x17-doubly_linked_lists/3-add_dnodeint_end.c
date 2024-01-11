9b6f714eb8e78705015b696c2b086fbd7da01ccd
Oloishorua <jacoboloishorua@gmail.com>
2024-01-12 01:05:35 +0300

    task 3 - func that adds elem to last node

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - func that adds node to end of list
+ * @head: pointer to head of list
+ * @n: integer to add to new node
+ * Return: address of new node or NULL
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, last_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	last_node = *head;
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+	last_node->next = new_node;
+	new_node->prev = last_node;
+
+	return (new_node);
+}
