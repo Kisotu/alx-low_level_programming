5e7b56bc3d03b0e0ad408db721f28604e2a8d771
Oloishorua <jacoboloishorua@gmail.com>
2024-01-12 00:28:39 +0300

    task 2 - func that adds node at beginning of list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - func that adds a node at beginning of list
+ * @head: pointer to head of list
+ * @n: integer of new node to contain
+ * Return: address of new node or NULL
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlist_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+
+	return (new_node);
+}
