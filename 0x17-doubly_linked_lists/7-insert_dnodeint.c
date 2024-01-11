31c74aeb167c1466c6fddb6c023c47dc07194053
Oloishorua <jacoboloishorua@gmail.com>
2024-01-12 01:42:28 +0300

    task 7 - func that inserts node at given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeoff(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
 	new_node->n = n;
 	new_node->prev = temp;
 	new_node->next = temp->next;
